a10e02e966540d6f162853431bddd78c4635e6fc
Ruperto A. Martinez <77865052+RupertoM@users.noreply.github.com>
2023-05-01 23:46:23 -0400

    var and if else statement

--- server.js
+++ server.js
@@ -3,3 +3,12 @@
 import minimist from 'minimist';
 import express from 'express';
 import {rps, rpsls} from './lib/rpsls.js';
+
+var argv = minimist(process.argv.slice(2));
+var HTTP_PORT = 0;
+
+if(argv['port'] != undefined){
+    HTTP_PORT = argv['port']
+} else {
+    HTTP_PORT = 5000;
+}
